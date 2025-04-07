203276ae5117df1dd8ba836d457bd41b4124f2b7
Reinaldo Neto <reinaldonetof@hotmail.com>
2025-04-07 02:20:00 -0300

    test: rename test suite to getBestMove and add additional test cases

--- __tests__/utils/minMaxAlgorithm.test.ts
+++ __tests__/utils/minMaxAlgorithm.test.ts
@@ -1,6 +1,6 @@
 import { getBestMove } from "../../src/utils/minMaxAlgorithm";
 
-describe("minMaxAlgorithm", () => {
+describe("getBestMove", () => {
   it("👤 vs 🤖 - should return best move 5", () => {
     const result = getBestMove({
       player1Selection: [1],
@@ -8,6 +8,23 @@ describe("minMaxAlgorithm", () => {
       cpuPosition: "player2",
     });
     expect(result).toBe(5);
+  });
+  it("👤 vs 🤖 - should return best move 3 for player2", () => {
+    const result = getBestMove({
+      player1Selection: [1, 5],
+      player2Selection: [2],
+      cpuPosition: "player2",
+    });
+    expect(result).toBe(3);
+  });
+
+  it("👤 vs 🤖 - should return best move 9 for player2", () => {
+    const result = getBestMove({
+      player1Selection: [1, 3, 6],
+      player2Selection: [2, 4],
+      cpuPosition: "player2",
+    });
+    expect(result).toBe(9);
   });
   it("🤖 vs 👤 - should return best move 7 when random is 0.6", () => {
     const mockMath = jest.spyOn(Math, "random").mockReturnValue(0.6);
@@ -19,5 +36,21 @@ describe("minMaxAlgorithm", () => {
     });
     expect(result).toBe(7);
     mockMath.mockRestore();
+  });
+  it("🤖 vs 👤 - should return best move 4 for player1", () => {
+    const result = getBestMove({
+      player1Selection: [5, 1],
+      player2Selection: [2, 9],
+      cpuPosition: "player1",
+    });
+    expect(result).toBe(4);
+  });
+  it("🤖 vs 👤 - should return best move 5 for player1", () => {
+    const result = getBestMove({
+      player1Selection: [1, 3],
+      player2Selection: [2, 4],
+      cpuPosition: "player1",
+    });
+    expect(result).toBe(5);
   });
 });
